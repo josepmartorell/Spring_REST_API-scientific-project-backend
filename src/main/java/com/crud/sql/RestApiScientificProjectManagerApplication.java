60cb7326ab4388489c4c2dd9cb4df66f5ce4245d
josepmartorell <martorelljosep>
2022-08-21 18:56:52 +0200

    start project

---
+++ src/main/java/com/crud/sql/RestApiScientificProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.crud.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiScientificProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiScientificProjectManagerApplication.class, args);
+	}
+
+}
